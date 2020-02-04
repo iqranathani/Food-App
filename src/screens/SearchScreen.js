eff8a5e19b0d9a133074590470dbac1c2397bf22
Iqra Nathani <iqranathani>
2020-02-04 20:12:52 +0500

    initial commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
+
+const SearchScreen = ({ navigation }) => {
+    const [term, setTerm] = useState('');
+    const [searchApi, results, errorMessage] = useResults();
+
+    const filterResultsByPrice = (price) => {
+        // price === '$' || '$$' || '$$$'
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
+
+    return (
+        <>
+            <SearchBar
+                term={term}
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(term)}
+            />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <ScrollView>
+              <ResultsList 
+               title="Cost Effective" 
+               results={filterResultsByPrice('$')}
+               navigation={navigation}
+              />
+             <ResultsList 
+               title="Bit Pricier" 
+               results={filterResultsByPrice('$$')}
+               navigation={navigation}
+              />
+              <ResultsList 
+               title="Big Spender" 
+               results={filterResultsByPrice('$$$')}
+               navigation={navigation}
+              />
+            </ScrollView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
